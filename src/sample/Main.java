e7d6fbd14adb1848972341aacf79859dc151d519
TforTrevor <36804391+TforTrevor@users.noreply.github.com>
2019-11-05 19:35:30 -0500

    Test javafx functionality

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,25 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        BorderPane root = new BorderPane();
+        Test test = new Test();
+        Button test2 = new Button("Test 2");
+        root.setCenter(test);
+        root.setRight(test2);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
