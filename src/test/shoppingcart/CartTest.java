b41bc9218e8befc45bd6b6c520ffdbd62588ee1d
aaronzygala <azygala2016@fau.edu>
2019-12-04 16:24:18 -0500

    Added unit tests for main classes

---
+++ src/test/shoppingcart/CartTest.java
@@ -0,0 +1,95 @@
+package test.shoppingcart; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+import shoppingcart.Cart;
+import shoppingcart.Item;
+
+/** 
+* Cart Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Dec 4, 2019</pre> 
+* @version 1.0 
+*/ 
+public class CartTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getCartSize() 
+* 
+*/ 
+@Test
+public void testGetCartSize() throws Exception {
+
+    System.out.println("Cart Size");
+    Cart instance = new Cart();
+    int expResult = 0;
+    int result = instance.getCartSize();
+    assert(result == (expResult));
+} 
+
+/** 
+* 
+* Method: addItem(Item item) 
+* 
+*/ 
+@Test
+public void testAddItem() throws Exception {
+    System.out.println("Add Item");
+    Item item = new Item(0, "", "", 0,0,0,"");
+    Cart instance = new Cart();
+    Cart expResult = new Cart();
+    expResult.addItem(item);
+    Cart result = instance;
+    result.addItem(item);
+    assert(result.getCartItems().equals(expResult.getCartItems()));
+} 
+
+/** 
+* 
+* Method: removeItem(Item item) 
+* 
+*/ 
+@Test
+public void testRemoveItem() throws Exception {
+    System.out.println("Remove Item");
+    Item item = new Item(0, "", "", 0,0,0,"");
+    Cart instance = new Cart();
+    instance.addItem(item);
+    Cart expResult = new Cart();
+    expResult.addItem(item);
+    expResult.removeItem(item);
+    Cart result = instance;
+    result.removeItem(item);
+    assert(result.getCartItems().equals(expResult.getCartItems()));
+} 
+
+/** 
+* 
+* Method: getCartItems() 
+* 
+*/ 
+@Test
+public void testGetCartItems() throws Exception {
+    System.out.println("Get Cart Items");
+    Item item = new Item(0, "", "", 0,0,0,"");
+    Cart instance = new Cart();
+    Cart expResult = new Cart();
+    expResult.addItem(item);
+    Cart result = instance;
+    result.addItem(item);
+    assert(result.getCartItems().equals(expResult.getCartItems()));
+} 
+
+
+} 
